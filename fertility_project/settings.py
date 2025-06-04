656235fe323ed8d390284feea13d356240998581
EmmanuelAttah1 <attahzuzu@gmail.com>
2025-06-04 09:52:23 +0100

    updated allowed host

--- fertility_project/settings.py
+++ fertility_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)-^jjz+bnjmeihaj=n0*ud6ah_)mzct1vni1vzlst%4-nk#1zq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
